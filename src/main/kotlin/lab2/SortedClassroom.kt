7ad2a09ef48b702c6f22f5b7a3293a878c913074
thenamskov <ahoryaw1@outlook.com>
2021-10-15 19:33:33 +0000

    added inner class for sorting

--- src/main/kotlin/lab2/SortedClassroom.kt
+++ src/main/kotlin/lab2/SortedClassroom.kt
@@ -2,6 +2,12 @@ package lab2
 
 class SortedClassroom(vararg sts : Student) : Classroom<Student>(*sts)  {
 
+    inner class StudentSorter<T>: Comparator<T> {
+
+        override fun compare(o1: T, o2: T): Int {
+            return 1
+        }
+    }
 
     fun addBetter() : Unit {
 
